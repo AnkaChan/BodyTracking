6f10aaad4420ac1985e45f1e62b6859382c5865c
AnkaChen <ankachan@126.com>
2020-06-16 11:41:59 -0600

    Generate personal shape. Silhouette fitting iwth personal shape.
    Signed-off-by: AnkaChen <ankachan@126.com>

---
+++ AC_ModelFitting/S04_TestPullBackPoints.py
@@ -0,0 +1,73 @@
+import sys
+import os
+import glob
+sys.path.append(os.path.abspath(''))
+
+from datetime import datetime
+
+SMPLSH_Dir = r'..\SMPL_reimp'
+
+import sys
+sys.path.insert(0, SMPLSH_Dir)
+import smplsh_torch
+
+from os.path import join
+import pyvista as pv
+import torch
+print(torch.version.cuda)
+
+
+import numpy as np
+from Utility import *
+
+if __name__ == '__main__':
+    fitParamFolder = r'F:\WorkingCopy2\2020_05_31_DifferentiableRendererRealData\Output\RealDataSilhouette\XYZ_RestPose_HHFix_Sig_1e-07_BR1e-07_Fpp6_NCams16ImS1080_LR0.4_LW1e-07_NW0.0_Batch4\FitParam'
+    smplshData = r'..\SMPL_reimp\SmplshModel_m.npz'
+    smplshExampleMeshFile = r'C:\Code\MyRepo\ChbCapture\06_Deformation\SMPL_Socks\SMPLSH\SMPLSH.obj'
+    personalShapeFile = r'F:\WorkingCopy2\2020_06_14_FitToMultipleCams\InitialFit\PersonalModel\PersonalShape.npy'
+    # interpolatedMeshFile =  r'F:\WorkingCopy2\2020_06_14_FitToMultipleCams\InitialFit\PersonalModel\InterpolatedWithSparse.ply'
+    interpolatedMeshFile =  r'F:\WorkingCopy2\2020_06_14_FitToMultipleCams\InitialFit\PersonalModel\FinalMesh.obj'
+
+    device = torch.device("cuda:0")
+    smplsh = smplsh_torch.SMPLModel(device, smplshData, personalShape=None, unitMM=True)
+
+    fitParamFiles= glob.glob(join(fitParamFolder, '*.npz'))
+    fitParamFiles.sort()
+    pose_size = 3 * 52
+
+    smplshExampleMesh = pv.PolyData(smplshExampleMeshFile)
+    outFolderMesh = r'F:\WorkingCopy2\2020_05_31_DifferentiableRendererRealData\Output\RealDataSilhouette\XYZ_RestPose_HHFix_Sig_1e-07_BR1e-07_Fpp6_NCams16ImS1080_LR0.4_LW1e-07_NW0.0_Batch4\RestposeChange'
+
+    param = np.load(fitParamFiles[-1])
+    # personalShapeFinal = param['personalShape']
+    trans = param['trans']
+    pose = param['pose']
+    beta = param['beta']
+
+    pose = torch.tensor(pose, dtype=torch.float64, requires_grad=True, device=device)
+    beta = torch.tensor(beta, dtype=torch.float64, requires_grad=True, device=device)
+    trans = torch.tensor(trans, dtype=torch.float64,
+                         requires_grad=True, device=device)
+    deformedVerts = smplsh(beta, pose, trans,)
+    T = smplsh.getTransformation(beta, pose, trans)
+
+    inverseTransform = np.zeros(T.shape, dtype=np.float64)
+
+    interpolatedMesh = pv.PolyData(interpolatedMeshFile)
+    interpolatedVerts = np.array(interpolatedMesh.points)
+    personalShapeFinalRestpose = np.zeros(interpolatedVerts.shape, dtype=np.float64)
+
+    for i in range(T.shape[0]):
+        inverseTransform[i, :, :] = np.linalg.inv(T[i, :, :].cpu().detach().numpy())
+        pt = interpolatedVerts[i:i+1, :].transpose()
+        pt = np.vstack([pt, 1])
+
+        ptBackToRest = inverseTransform[i, :, :]  @ pt
+        personalShapeFinalRestpose[i, :] = ptBackToRest[:3, 0]
+
+    interpolatedMesh.points = personalShapeFinalRestpose
+    interpolatedMesh.save('DisplacementToRestpose.ply')
+
+    v_shaped = torch.tensordot(smplsh.shapedirs, beta, dims=([2], [0])) + smplsh.v_template
+    personalShapeFinal = personalShapeFinalRestpose - v_shaped.cpu().detach().numpy()
+    np.save(personalShapeFile, personalShapeFinal)
